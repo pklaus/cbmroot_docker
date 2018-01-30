2695b3819fe7f91e7ca0051cb811750182efecc9
Philipp Klaus <philipp.klaus@gmail.com>
2018-01-30 16:41:13 +0100

    multistage build: build one --target at a time; fix tagging

--- opensuse-42.3/build.sh
+++ opensuse-42.3/build.sh
@@ -3,17 +3,17 @@
 set -e # fail if any command fails
 
 # build
-time \
-   docker build \
+for TARGET in fairsoft fairroot cbmroot
+do
+   time docker build \
    --build-arg fairsoft_branch=mar17 \
    --build-arg fairroot_branch=v-17.03a \
    --build-arg cbmroot_release=DEC17 \
-   -t cbmroot .
+   --target $TARGET \
+   -t $TARGET .
+done
 
 # tag
-FAIRSOFT_IMAGE=$(docker images --filter "label=build=fairsoft" --format '{{.CreatedAt}}\t{{.ID}}' | sort -nr | head -n 1 | cut -f2)
-FAIRROOT_IMAGE=$(docker images --filter "label=build=fairroot" --format '{{.CreatedAt}}\t{{.ID}}' | sort -nr | head -n 1 | cut -f2)
-CBMROOT_IMAGE=$( docker images --filter "label=build=cbmroot"  --format '{{.CreatedAt}}\t{{.ID}}' | sort -nr | head -n 1 | cut -f2)
-docker tag $FAIRSOFT_IMAGE pklaus/fairsoft:opensuse-42.3_mar17
-docker tag $FAIRROOT_IMAGE pklaus/fairroot:opensuse-42.3_v-17.03a
-docker tag $CBMROOT_IMAGE  pklaus/cbmroot:opensuse-42.3_DEC17
+docker tag fairsoft pklaus/fairsoft:opensuse-42.3_mar17
+docker tag fairroot pklaus/fairroot:opensuse-42.3_v-17.03a
+docker tag cbmroot  pklaus/cbmroot:opensuse-42.3_DEC17
