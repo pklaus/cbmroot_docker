7dae735bf8a05c478fb00b002be5b548de76b70a
Philipp Klaus <philipp.l.klaus@web.de>
2019-02-13 14:05:01 +0100

    Suse 15.1 OCT18: fix versions in push.sh

--- opensuse-15.1/push.sh
+++ opensuse-15.1/push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # push
-docker push pklaus/fairsoft:opensuse-15.1_may18
-docker push pklaus/fairroot:opensuse-15.1_v18.0.4
+docker push pklaus/fairsoft:opensuse-15.1_oct17p1
+docker push pklaus/fairroot:opensuse-15.1_v-17.10c
 docker push pklaus/cbmroot:opensuse-15.1_OCT18
 
