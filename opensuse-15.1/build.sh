fefddd02c0e8b849b3ba2dec1a4d2d2b522d1ba6
Philipp Klaus <philipp.l.klaus@web.de>
2019-02-13 13:23:10 +0100

    OCT18 is only working w/ older dependency set

--- opensuse-15.1/build.sh
+++ opensuse-15.1/build.sh
@@ -6,14 +6,14 @@ set -e # fail if any command fails
 for TARGET in fairsoft fairroot cbmroot
 do
    time docker build \
-   --build-arg fairsoft_branch=may18 \
-   --build-arg fairroot_branch=v18.0.4 \
+   --build-arg fairsoft_branch=oct17p1 \
+   --build-arg fairroot_branch=v-17.10c \
    --build-arg cbmroot_release=OCT18 \
    --target $TARGET \
    -t $TARGET .
 done
 
 # tag
-docker tag fairsoft pklaus/fairsoft:opensuse-15.1_may18
-docker tag fairroot pklaus/fairroot:opensuse-15.1_v18.0.4
+docker tag fairsoft pklaus/fairsoft:opensuse-15.1_oct17p1
+docker tag fairroot pklaus/fairroot:opensuse-15.1_v-17.10c
 docker tag cbmroot  pklaus/cbmroot:opensuse-15.1_OCT18
